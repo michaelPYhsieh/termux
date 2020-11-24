9f0a10cb20f81481717f856060229d13b29e4b15
Michael Hsieh <m9810223@gmail.com>
2020-11-24 23:33:40 +0800

    +

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
-apt-get install -y \
-    wget proot vim openssh git && \
-./ubuntu.sh -y
-
-
 echo "cd && \
 cd termux && \
 ./startubuntu.sh" >> ~/.bash_profile && \
+apt-get install -y \
+    wget proot vim openssh git && \
+./ubuntu.sh -y && \
 ./startubuntu.sh
